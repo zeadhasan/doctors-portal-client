bb6c70c27ffb65fa8627d79521026591c1e18224
zeadhasan <zeadhasan1995shuvo@gmail.com>
2022-06-10 01:19:48 +0600

    banner part number one commit

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
-            <h4>This is Home Page</h4>
+            <Banner></Banner>
         </div>
     );
 };
