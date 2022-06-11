fb080b3f98518cd2791902b26337d057dae15ab3
zeadhasan <zeadhasan1995shuvo@gmail.com>
2022-06-11 11:14:33 +0600

    InfoCart Dynamic commit

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import InfoCart from './InfoCart';
 import clock from '../../assets/icons/clock.svg';
+import marker from '../../assets/icons/marker.svg';
+import phone from '../../assets/icons/phone.svg';
 
 const Info = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
-            <InfoCart img={clock}></InfoCart>
-            <InfoCart img={clock}></InfoCart>
-            <InfoCart img={clock}></InfoCart>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 px-12'>
+            <InfoCart bgClass="bg-gradient-to-r from-secondary to-primary" cartMore="Lorem Ipsum is simply dummy text of the pri" cardTitle="Opening Hours" img={clock}></InfoCart>
+            <InfoCart bgClass="bg-accent" cartMore="Brooklyn, NY 10036, United States" cardTitle="Visit our location" img={marker}></InfoCart>
+            <InfoCart bgClass="bg-gradient-to-r from-secondary to-primary" cartMore="+000 123 456789" cardTitle="Contact us now" img={phone}></InfoCart>
         </div>
     );
 };
--- src/Pages/Home/InfoCart.js
+++ src/Pages/Home/InfoCart.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const InfoCart = ({ img }) => {
+const InfoCart = ({ img, cardTitle, cartMore, bgClass }) => {
     return (
-        <div class="card lg:card-side shadow-xl bg-accent">
-            <figure><img src={img} alt=" Album" /></figure>
-            <div class="card-body">
-                <h2 class="card-title">New album is released!</h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
+        <div class={`card lg:card-side shadow-xl ${bgClass}`}>
+            <figure className='pl-5'><img width={40} src={img} alt=" Album" /></figure>
+            <div class="card-body text-white">
+                <h2 class="card-title ">{cardTitle}</h2>
+                <p className='text-sm'>{cartMore}</p>
             </div>
         </div>
     );
