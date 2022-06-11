69f5c5ef6a6a344e2b6fe12616f7a1b8642728cc
zeadhasan <zeadhasan1995shuvo@gmail.com>
2022-06-11 10:36:08 +0600

    Info setup commit

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import InfoCart from './InfoCart';
+import clock from '../../assets/icons/clock.svg';
 
 const Info = () => {
     return (
-        <div>
-
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
+            <InfoCart img={clock}></InfoCart>
+            <InfoCart img={clock}></InfoCart>
+            <InfoCart img={clock}></InfoCart>
         </div>
     );
 };
--- src/Pages/Home/InfoCart.js
+++ src/Pages/Home/InfoCart.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const InfoCart = () => {
+const InfoCart = ({ img }) => {
     return (
-        <div>
-
+        <div class="card lg:card-side shadow-xl bg-accent">
+            <figure><img src={img} alt=" Album" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">New album is released!</h2>
+                <p>Click the button to listen on Spotiwhy app.</p>
+            </div>
         </div>
     );
 };
