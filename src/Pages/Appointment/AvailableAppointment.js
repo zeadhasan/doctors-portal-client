48dd5940b5ca88aaf19b8f29b568afcb6407f375
zeadhasan <zeadhasan1995shuvo@gmail.com>
2022-06-16 15:09:31 +0600

    set services commit

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,11 +1,25 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const AvailableAppointment = ({ date }) => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, []);
+
+
     return (
-        <div>
-            <h4 className='text-xl text-primary text-center p-4'> Available Appointments No {format(date, 'PP')}</h4>
-        </div>
+        <section>
+            <div>
+                <h4 className='text-xl text-primary text-center p-4'> Available Appointments No {format(date, 'PP')}</h4>
+                <div>
+
+                </div>
+            </div>
+        </section>
     );
 };
 
