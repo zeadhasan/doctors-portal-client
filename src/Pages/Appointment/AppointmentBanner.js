cc40a1fa87c646c9c2d12d26f255b9265142b088
zeadhasan <zeadhasan1995shuvo@gmail.com>
2022-06-16 12:20:38 +0600

    date paker commit

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import chair from '../../assets/images/chair.png';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
+import { format } from 'date-fns';
 
 const AppointmentBanner = () => {
+    const [date, setDate] = useState(new Date());
     return (
         <div className="hero min-h-screen">
             <div className='px-12'>
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt="Doctor-Chair" />
                     <div>
-                        <DayPicker />
+                        <DayPicker
+                            mode="single"
+                            selected={date}
+                            onSelect={setDate}
+                        />
+                        <p>You Have Selected: {format(date, 'PP')}</p>
                     </div>
                 </div>
             </div>
