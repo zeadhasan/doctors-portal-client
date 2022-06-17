882ecdb3fc7b3c73b0d6740329968c0bb336ed3f
zeadhasan <zeadhasan1995shuvo@gmail.com>
2022-06-17 15:59:39 +0600

    login page setup

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,38 +4,17 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    </div>
-                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <div className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
-                                <label class="label">
-                                    <Link to="" className="label-text-alt link link-hover">Forgot password?</Link>
-                                </label>
-                            </div>
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
-                            </div>
-                        </div>
+        <section className='px-12 h-screen flex justify-center items-center'>
+            <div className="card w-96 bg-base-100 shadow-xl ">
+                <div className="card-body">
+                    <h2 className="text-3xl font-bold text-center text-secondary">Login</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
