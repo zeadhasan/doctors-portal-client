4633244c8e60d784ac02cb7b12bc88559e75c0ff
zeadhasan <zeadhasan1995shuvo@gmail.com>
2022-06-18 11:33:50 +0600

    implement name and  sign out

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
     const [createUserWithEmailAndPassword, user, loading, error,] = useCreateUserWithEmailAndPassword(auth);
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+    const navigate = useNavigate()
 
     let signInError;
 
@@ -15,17 +17,19 @@ const SignUp = () => {
         console.log(user);
     }
 
-    if (errors) {
-        signInError = <p className="text-red-700 mb-3"><small>{error?.message}</small></p>
+    if (errors || updateError) {
+        signInError = <p className="text-red-700 mb-3"><small>{error?.message || updateError?.message}</small></p>
     }
 
-    if (loading) {
+    if (loading || updating) {
         return <Loading></Loading>
     }
 
-    const onSubmit = data => {
-        console.log(data);
-        createUserWithEmailAndPassword(data.email, data.password);
+    const onSubmit = async data => {
+        await createUserWithEmailAndPassword(data.email, data.password);
+        await updateProfile({ displayName: data.name });
+        console.log("update Done");
+        navigate("/appointment");
     };
     return (
         <section className='px-12 h-screen flex justify-center items-center'>
