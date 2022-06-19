ce38ba0c10004f6017545443e011c5f0ab0079ed
zeadhasan <zeadhasan1995shuvo@gmail.com>
2022-06-19 15:31:00 +0600

    bookig data sent to mongoDB

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -2,17 +2,41 @@ import React from 'react';
 import { format } from 'date-fns';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import { toast } from 'react-toastify';
 
 
 const BookingModal = ({ date, treatment, setTreatment }) => {
     const { _id, name, slots } = treatment;
     const [user] = useAuthState(auth);
+    const formattedDate = format(date, 'PP');
 
     const handleBooking = (event) => {
         event.preventDefault();
         const slot = event.target.slot.value;
-        console.log(_id, name, slot);
-        setTreatment(null);
+
+        const booking = {
+            treatmentId: _id,
+            treatment: name,
+            date: formattedDate,
+            slot,
+            patient: user.email,
+            patientName: user.displayName,
+            phone: event.target.phone.value
+        }
+        fetch('http://localhost:5000/booking', {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(booking)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setTreatment(null);
+            })
+
+
     }
 
     return (
@@ -31,7 +55,7 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
                         </select>
                         <input type="text" name="name" disabled value={user?.displayName || ""} className="input input-bordered w-full max-w-xs" />
                         <input type="email" name='email' disabled value={user?.email || ""} className="input input-bordered w-full max-w-xs" />
-                        <input type="text" name='number' placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
+                        <input type="text" name='phone' placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
                         <input type="submit" value="Submit" className=" btn-sm btn btn-accent input-bordered w-full max-w-xs" />
                     </form>
                 </div>
